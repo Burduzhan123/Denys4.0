5ec1f0412a22e62d9985169a16341cd52b667372
Denys <denisburduzan@gmail.com>
2023-12-05 17:58:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const condition = 2023
+
+switch (condition){
+    case 2023:
+        console.log('Leap year');
+        break;
+    case 2022:
+        console.log('Not leap year');
+        break;
+    case 2021:
+        console.log('Not leap year');
+        break;
+}
